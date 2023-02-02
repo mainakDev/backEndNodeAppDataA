9e2373a16f2463c9ba4fcc146d069164917f328b
Mainak Dev <mainak@mainak.local>
2023-02-02 11:41:48 +0530

    Feb 2 2022 final push

--- 02_02_2023/app.js
+++ 02_02_2023/app.js
@@ -2,8 +2,22 @@ const express = require('express'); //import express
 
 const app = express();
  
-app.use((req, res) => {
+//middleware1
+app.use('/',(req, res, next) => {
     console.log('Inside Middleware function');
+    next();
+});
+
+app.use('/addProduct',(req, res) => {
+    res.send('<h1>Add Product Page</h1>');
+});
+
+app.use('/products',(req, res) => {
+    res.send('<h1>Products Page</h1>');
+});
+
+app.use('/',(req, res) => {
+    res.send('<h1>Hello from Express</h1>');
 });
 
 app.listen(3000);
