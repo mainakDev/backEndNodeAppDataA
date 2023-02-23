51196bf3d98c4eb3eb42b1fe74f7e28abd8f22c4
Mainak Dev <mainak@mainak.local>
2023-02-23 10:35:53 +0530

    Feb 23

--- 14_02_2023/routes/admin.js
+++ 14_02_2023/routes/admin.js
@@ -4,17 +4,18 @@ const express = require('express');
 
 const router = express.Router();
 
-//addProduct middleware
-//localhost:3000/admin/add-products
-router.get('/add-Products',(req, res, next) => {
-    res.sendFile(path.join(__dirname,'../', 'views/', 'add-products.html'));
+const rootDir = require('../util/path');
+
+//addProduct middleware localhost:3000/admin/add-products
+router.get('/add-products',(req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views/', 'add-products.html'));
 });
 
 //products middleware
 //localhost:3000/admin/products
 router.post('/products',(req, res, next) => {
     console.log(req.body);
-    res.sendFile(path.join(__dirname,'../', 'views/', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views/', 'shop.html'));
 });
 
 module.exports = router;
